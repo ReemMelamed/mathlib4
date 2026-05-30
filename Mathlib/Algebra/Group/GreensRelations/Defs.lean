f16337ae6573733b883b510879e5afb696cb8b21
ReemMelamed <reem.melamed@campus.technion.ac.il>
2026-05-30 19:29:57 +0300

    fix

--- Mathlib/Algebra/Group/GreensRelations/Defs.lean
+++ Mathlib/Algebra/Group/GreensRelations/Defs.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 Re'em Melamed-Katz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Re'em Melamed-Katz
 -/
+module
 
 import Mathlib.Algebra.Group.Basic
 
